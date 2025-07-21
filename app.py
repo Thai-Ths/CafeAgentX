ba030ff54185372e26120a3091537c4d2ea5419b
Thai Sutthiwattanakarn <102790509+Thai-Ths@users.noreply.github.com>
2025-07-21 22:30:29 +0700

    app.py for Gradio

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     # Optionally: sync database from CSVs on startup
     # csvs_to_sqlite()  # Uncomment if you want to auto-sync DB
     app = create_app(chat, clear_chat)
-    app.launch(debug=settings.DEBUG) 
+    app.launch(debug=settings.DEBUG)
